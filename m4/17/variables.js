0a872b727fe3a8cb88973b315172e944283035b2
Shahadat <shahadat2cp17@gmail.com>
2022-01-25 22:52:30 +0600

    17-3

--- m4/17/variables.js
+++ m4/17/variables.js
@@ -1,3 +1,15 @@
+// 1
+console.log("Hello World!")
+
+// 2 Numeric
 var price = 21;
 var age = 28;
 var temperature = 37;
+
+// String
+var name = "Shahadat";
+var fullName = "Md Shahadat Hossain";
+
+// Boolean
+var pass = true;
+var subcribeed = false
