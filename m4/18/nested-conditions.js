8d70cc6edbadd6d0268fcc2f10e231a7aa95a5a6
Shahadat <shahadat2cp17@gmail.com>
2022-01-27 15:14:41 +0600

    17 com

---
+++ m4/18/nested-conditions.js
@@ -0,0 +1,13 @@
+var danishP = 50;
+var butterP = 25;
+var toastP = 10;
+var budget = 100; 
+var packedWell = false;
+
+if (danishP < budget){
+    if(packedWell == true){
+    console.log("khabo");
+}else {
+    console.log("Khamu na!");
+}
+}
---
+++ m4/18/summary.js
@@ -0,0 +1,9 @@
+console.log(6 != 6)
+console.log(6 >= 6)
+
+var arr =  [3,7,10]
+console.log(arr[1])
+
+var nums = [1,5,8]
+console.log(nums.indexOf(0))
+console.log(nums.indexOf(1))
