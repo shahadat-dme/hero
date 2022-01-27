716f835ccac2b75c45e3740cf77f8e98584a7831
Shahadat <shahadat2cp17@gmail.com>
2022-01-27 07:14:44 +0600

    index

--- m4/18/array.js
+++ m4/18/array.js
@@ -1,5 +1,26 @@
 var arr = [34,55,85]
 console.log(arr)
 
-var name = ["Md", "shahadat", "Hossain"]
-console.log(name)
+var nam = ["Md", "shahadat", "Hossain"]
+console.log(nam)
+console.log(nam.length)
+
+var books = ['hablu','facebook','google','amazon']
+var list = books.indexOf("facebook")
+console.log(list)
+
+var n = [1,2,33,4,5]
+console.log(n.indexOf(3))
+
+var third = n[3]
+console.log(third)
+
+var index = n[122]
+console.log(index)
+
+var index1 = n.indexOf[122]
+console.log(index1)
+
+console.log(n)
+n[1]=7
+console.log(n)
