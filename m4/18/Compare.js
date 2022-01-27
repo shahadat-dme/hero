bbab7be22d188e0cdf364b664b154786f8e87b9a
Shahadat <shahadat2cp17@gmail.com>
2022-01-27 10:13:19 +0600

    arr push pop

---
+++ m4/18/Compare.js
@@ -0,0 +1,15 @@
+console.log(5 > 7)
+console.log(5 < 7)
+
+console.log(8 == 8)
+
+console.log(8 == "8")
+
+console.log(8 === "8")
+
+console.log(8 === 8)
+console.log(8 != 8)
+console.log(8 != 7)
+
+console.log("good" && "best")
+console.log("love" || "hate")
---
+++ m4/18/push.js
@@ -0,0 +1,5 @@
+var lastBrench = ['kalam', 'balam', 'salam']
+lastBrench.push("kulam")
+console.log(lastBrench)
+lastBrench.pop()
+console.log(lastBrench)
