33b02c28f08ded8994e731a42695498dc4a5fdd6
Shahadat <shahadat2cp17@gmail.com>
2022-01-29 01:10:14 +0600

    func

---
+++ m4/19/parameter.js
@@ -0,0 +1,16 @@
+// function bringSingara(taka){
+//     console.log("singara den",taka);
+//     console.log("Singara Koi");
+//     var singaraPrice = 10;
+//     var singaraQuality = taka / singaraPrice
+//     return singaraQuality
+// }
+// var money = 250;
+// var singara = bringSingara(money)
+// console.log(singara);
+
+function sum(n,m){
+    return n+m
+}
+
+console.log(sum(7,8));
