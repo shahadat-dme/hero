9eb96f103b21d4d80c60e619838841d99bd63e7e
Shahadat <shahadat2cp17@gmail.com>
2022-01-29 14:50:22 +0600

    switch

---
+++ m4/19/switch-case.js
@@ -0,0 +1,18 @@
+var color = 'yellow'
+
+switch (color){
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    case 'green':
+        console.log("color is green");
+        break;
+    default:
+        console.log('color is black');
+}
