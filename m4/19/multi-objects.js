8c089fda314e2c0d5defeafa1f9f5fdf6093c4be
Shahadat <shahadat2cp17@gmail.com>
2022-01-29 12:30:40 +0600

    objects

---
+++ m4/19/multi-objects.js
@@ -0,0 +1,27 @@
+var student1 ={
+    id: 101,
+    name:"Shahadat",
+    marks: 89,
+    phone: 0011174
+}
+
+var mobile = {
+    color: 'black',
+    price: 17000,
+    screen: 5.5,
+    storage: '16GB'
+}
+// console.log(student1,mobile)
+
+// console.log(mobile.price)
+
+mobile.price = 15000;
+console.log(mobile)
+
+
+// property
+// var priceProperty = 'price'
+
+// computer.price = 22000;
+// computer["price"] = 23000;
+// computer[priceProperty] = 19000;
---
+++ m4/19/multi-parameter.js
@@ -0,0 +1,6 @@
+function add(num1 , num2){
+    console.log(num1 + num2)
+    // return num1 * num2
+}
+add(85,95)
+// console.log(add(9,8))
