b469029d2394bc58d3a5cbcebc360e3e0bc15f8d
Shahadat <shahadat2cp17@gmail.com>
2022-01-30 16:19:26 +0600

    for

---
+++ m4/20/factorial-while.js
@@ -0,0 +1,40 @@
+// function getFactorial(number){
+//     let factorial = 1;
+//     let i = 1;
+//     while (i <= number){
+//         factorial = factorial * i;
+//         i++;
+//     }
+//     return factorial;
+// }
+
+// const myFactorial = getFactorial(6)
+// console.log(myFactorial);
+
+
+
+/*
+function getFactorial(number){
+    let factorial = 1;
+    let i = number;
+    while (i >= 1){
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial;
+}
+
+const myFactorial = getFactorial(6)
+console.log(myFactorial);
+*/
+
+function getFactorial(number){
+    let factorial = 1;
+    for (let i = number; i >= 1; i--){
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+const myFactorial = getFactorial(6)
+console.log(myFactorial);
