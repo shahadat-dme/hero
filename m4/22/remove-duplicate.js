a9dc55c5718edeba415926bef2fe23d10ce7b0fa
Shahadat <shahadat2cp17@gmail.com>
2022-02-02 12:37:40 +0600

    remove

---
+++ m4/22/remove-duplicate.js
@@ -0,0 +1,18 @@
+const names = ['abul','babul','cabul','dabul','abul','gabul','babul']
+function removeDuplicate(names){
+    const unique = [];
+
+    // for(let i = 0; i < names.length; i++){
+    //     const element = names[i];
+    //     console.log(element);
+    // }
+    for(const element of names){
+        console.log(element)
+        if(unique.indexOf(element) == -1){
+            unique.push(element)
+        }
+    }
+    return unique
+}
+const uniqueNames = removeDuplicate(names)
+console.log(uniqueNames);
