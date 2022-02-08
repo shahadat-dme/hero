cd77e935fda3850241770d973331f5c38ccde14d
Shahadat <shahadat2cp17@gmail.com>
2022-02-08 12:55:18 +0600

    event

--- m5/25/script.js
+++ m5/25/script.js
@@ -52,9 +52,79 @@ document.getElementById('handle-event').addEventListener('click',function(){
 
 document.getElementById('update-name').addEventListener('click',function(){
     const nameField = document.getElementById('name-field');
-    const clickingMethod = document.getElementById('clicking-method');
+    const clickingMethod = document.getElementById('input');
     clickingMethod.innerText = nameField.value
     nameField.value = '';
     // nameField.innerText = 'set by Direct event lister'
     // console.log(nameField.value);
+})
+
+
+//comment add in page on textarea
+document.getElementById('submit-comment').addEventListener('click',function(){
+    const commentBox = document.getElementById('new-comment');
+    // const userComment = commentBox.value
+    const newComment = document.createElement('p');
+    newComment.innerText = commentBox.value;
+
+    const commentContainer = document.getElementById('comment-container');
+    commentContainer.appendChild(newComment)
+    commentBox.value = '';
+})
+
+
+// Delete Button
+document.getElementById('delete-btn').addEventListener('click',function(){
+    document.getElementById('secret-info').style.display = 'none'
+})
+
+//focus
+document.getElementById('delete-confirm').addEventListener('focus',function(){
+    document.body.style.backgroundColor = 'lightcoral';
+    // document.getElementById('secret-info').style.display = 'none'
+})
+
+//blur
+document.getElementById('delete-confirm').addEventListener('blur',function(){
+    document.body.style.backgroundColor = 'white';
+    // document.getElementById('secret-info').style.display = 'none'
+})
+
+//keydown
+// document.getElementById('delete-confirm').addEventListener('keydown',function(){
+//     console.log('keydown');
+//     const deleteField = document.getElementById('delete-confirm');
+//     console.log(deleteField.value)
+// })
+
+
+//keypress
+// document.getElementById('delete-confirm').addEventListener('keypress',function(){
+//     console.log('keypress');
+//     const deleteField = document.getElementById('delete-confirm');
+//     console.log(deleteField.value)
+// })
+
+//keyup
+// document.getElementById('delete-confirm').addEventListener('keyup',function(){
+//     const deleteField = document.getElementById('delete-confirm');
+//     console.log(deleteField.value)
+// })
+
+
+document.getElementById('delete-confirm').addEventListener('keyup',function(event){
+    // const deleteField = document.getElementById('delete-confirm');
+    const deleteBtn = document.getElementById('delete-btn')
+    if(event.target.value == 'delete'){
+        deleteBtn.removeAttribute('disabled')
+    }
+    else{
+        deleteBtn.setAttribute('disabled',true)
+    }
+})
+
+// change
+document.getElementById('delete-confirm').addEventListener('change',function(){
+    const deleteField = document.getElementById('delete-confirm');
+    console.log(deleteField.value)
 })
