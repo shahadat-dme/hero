11e103d3ded67e63c2e0e551dcbd39acb22c7b6f
Shahadat <shahadat2cp17@gmail.com>
2022-02-09 19:51:54 +0600

    element

--- m5/24/script.js
+++ m5/24/script.js
@@ -3,3 +3,16 @@ const li =  document.createElement('li')
 li.innerText = 'Blog-4';
 const ul = document.getElementById("blog-list")
 ul.appendChild(li);
+
+// let p = document.createElement('p')
+// console.log(p);
+
+const h1 = document.createElement("h1");
+const underline = document.createElement("u");
+underline.innerText = "Hello World!";
+h1.append(underline);
+document.body.append(h1)
+
+const h2 = document.createElement("h2");
+h2.innerText = "Bangladesh"
+document.body.append(h2)
