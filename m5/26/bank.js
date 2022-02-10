cc41580f2fc382854ec7f74732ecad368a3255f8
Shahadat <shahadat2cp17@gmail.com>
2022-02-11 01:39:33 +0600

    bank own

--- m5/26/bank.js
+++ m5/26/bank.js
@@ -20,3 +20,24 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     balanceTotal.innerText = newBalanceTotal;
     depositInput.value = '';
     })
+
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    // console.log('withdraw');
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    // console.log(withdrawAmountText);
+    const newWithdrawAmount = parseFloat(withdrawAmountText)
+    // console.log(newWithdrawAmount);
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+    withdrawInput.value = '';
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText)
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+})
