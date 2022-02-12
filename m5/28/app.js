230a509265e018516f98c49b83d60f3322927ba5
Shahadat <shahadat2cp17@gmail.com>
2022-02-12 23:59:17 +0600

    28

--- m5/28/app.js
+++ m5/28/app.js
@@ -3,3 +3,9 @@ document.getElementById('case-plus').addEventListener('click',function(){
     const caseNumber = caseInput.value;
     caseInput.value = parseInt(caseNumber) + 1;
 })
+
+document.getElementById('case-minus').addEventListener('click',function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+})
