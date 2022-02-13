bea13a7aebf29d7c0fec2a7e80b8873cc1b6977c
Shahadat <shahadat2cp17@gmail.com>
2022-02-13 12:43:21 +0600

    shoping cart

--- m5/28/app3.js
+++ m5/28/app3.js
@@ -9,23 +9,6 @@ function updateProductNumber(product, price, isIncreasing){
     productInput.value = productNumber;
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price
-    calculateTotal();
-}
-function getInputValue(product){
-    const productInput = document.getElementById(product + '-number');
-    const productNumber = parseInt(productInput.value);
-    return productNumber;
-}
-function calculateTotal(){
-    const phoneTotal = getInputValue('phone') * 1219;
-    const caseTotal = getInputValue('case') * 59;
-    const subTotal = phoneTotal + caseTotal;
-    const tax = subTotal / 10;
-    const totalPrice = subTotal + tax;
-
-    document.getElementById('sub-total').innerText = subTotal;
-    document.getElementById('tax-amount').innerText = tax;
-    document.getElementById('total-price').innerText = totalPrice;
 }
 document.getElementById('phone-plus').addEventListener('click',function(){
     updateProductNumber('phone', 1219, true);
