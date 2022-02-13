7f0be43b060bc86b50c8bc0600268d6782353df1
Shahadat <shahadat2cp17@gmail.com>
2022-02-13 19:38:58 +0600

    quiz

---
+++ m5/28/quiz.js
@@ -0,0 +1,31 @@
+// let count = 0;
+// console.log(parseInt('count'+1));
+
+// console.log(Math.pow(4, 0.5)+"spiderman"+"987"+Math.sqrt(9) );
+
+
+// function generatePin(){ 
+//     return Math.floor(Math.random()*90000) + 10000; 
+// }
+// console.log(generatePin());
+
+// console.log(!isNaN("10X"));
+
+// const product = 5; 
+// const price = "7"; 
+// const subTotal = product * price; 
+// const tax = subTotal/10; 
+// const total = subTotal + tax
+// console.log(total);
+
+const category="pen"; 
+const performer = category+"-teller";
+console.log(performer);
+
+{/* <span><h1>Heading is a block-level element</h1><span></span>
+<button id="increment-btn">3</button>
+<script>
+ const incrementButton = document.getElementById("increment-btn")
+ incrementButton.addEventListener("click", function () { 
+ incrementButton.innerText = parseInt(incrementButton.innerText)+3; 
+ })  */}
