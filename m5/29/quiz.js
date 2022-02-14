4261088b864f5d91cbbc263bedad7d0bf452c024
Shahadat <shahadat2cp17@gmail.com>
2022-02-14 21:39:18 +0600

    quiz

---
+++ m5/29/quiz.js
@@ -0,0 +1,32 @@
+// let prem = "i love you, javascript";
+// prem = prem.toUpperCase();
+// console.log(prem);
+
+// const task = "Practice Makes a person perfect";
+// const index = task.toLowerCase().indexOf("M");
+// console.log(index);
+
+// const str1 = 'Bangladesh is a beautiful country.'; 
+// console.log(str1.endsWith("country"));
+
+// const str= 'I love coding more than eating.';
+// console.log(str.slice(2,7));
+
+// const names =["rafiq", "jabbar","bakkar", "habib", "dalim"];
+// names.reverse()
+// console.log(names);
+
+// for (let i==0; i<10;i++){
+//     console.log(i);
+// }
+
+// const str1 = "Sh"
+// const str2 = "Md"
+// console.log(str2.concat(str1));
+
+// console.log(Date.now());
+// console.log(Date());
+
+const array = ["121", "12", "1", "112", "111"];
+
+console.log(array.sort());
