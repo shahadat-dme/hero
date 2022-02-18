7bc5ddabcae9a95cdcc5a26a50250bb13e4c3320
Shahadat <shahadat2cp17@gmail.com>
2022-02-18 16:35:58 +0600

    addition

--- m5/30.5/functional.js
+++ m5/30.5/functional.js
@@ -5,34 +5,65 @@ function getInputValue(fieldId){
     inputField.value = '';
     return value;
 }
-
-function updateTotal(fieldId,amount){
-    const totalTag = document.getElementById(fieldId);
-    const previousTotalInText = totalTag.innerText;
-    const previousTotal = parseFloat(previousTotalInText)
-    const newTotal = previousTotal + amount
-    totalTag.innerText = newTotal;
+function getInnerTextValue(fieldId){
+    const fieldTag = document.getElementById(fieldId)
+    const fieldValueText = fieldTag.innerText;
+    const value = parseFloat(fieldValueText)
+    return value;
 }
+// function updateTotal(fieldId,amount){
+//     const totalTag = document.getElementById(fieldId);
+//     const previousTotalInText = totalTag.innerText;
+//     const previousTotal = parseFloat(previousTotalInText)
+//     const newTotal = previousTotal + amount
+//     totalTag.innerText = newTotal;
+// }
+function updateTotal(fieldId,amount){
+    // const totalTag = document.getElementById(fieldId);
+    // const previousTotalInText = totalTag.innerText;
+    // const previousTotal = parseFloat(previousTotalInText)
+    const previousTotal = getInnerTextValue(fieldId)
+    const newTotal = previousTotal + amount
+    document.getElementById(fieldId).innerText = newTotal;
+    return newTotal;
+}
+// function updateBalance(amount, isAdding){
+//     const balanceTag = document.getElementById('balance-total');
+//     const balanceInText = balanceTag.innerText;
+//     const previousBalance = parseFloat(balanceInText)
+//     let newBalance;
+//     if(isAdding == true){
+//         newBalance = previousBalance + amount;
+//     }else{
+//         newBalance = previousBalance - amount;
+//     }
+//     balanceTag.innerText = newBalance;
+// }
 function updateBalance(amount, isAdding){
-    const balanceTag = document.getElementById('balance-total');
-    const balanceInText = balanceTag.innerText;
-    const previousBalance = parseFloat(balanceInText)
+    // const balanceTag = document.getElementById('balance-total');
+    // const balanceInText = balanceTag.innerText;
+    // const previousBalance = parseFloat(balanceInText)
+    const previousBalance = getInnerTextValue('balance-total')
     let newBalance;
     if(isAdding == true){
         newBalance = previousBalance + amount;
     }else{
         newBalance = previousBalance - amount;
     }
-    balanceTag.innerText = newBalance;
+    getElementById('balance-total').innerText = newBalance;
 }
 document.getElementById('deposit-button').addEventListener('click',function(){
     const amount = getInputValue('deposit-input')
-    updateTotal('deposit-total',amount)
-    updateBalance(amount,true);
+    if(amount > 0){
+        updateTotal('deposit-total',amount)
+        updateBalance(amount,true);
+    }
 })
 
 document.getElementById('withdraw-button').addEventListener('click',function(){
     const amount = getInputValue('withdraw-input')
-    updateTotal('withdraw-input', amount)
-    updateBalance(amount, false);
+    if(amount > 0){
+        updateTotal('withdraw-input', amount)
+        updateBalance(amount, false);
+    }
 })
