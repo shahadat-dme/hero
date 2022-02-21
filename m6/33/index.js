36532eed0f17745c72e3ed42a069176e45335d85
Shahadat <shahadat2cp17@gmail.com>
2022-02-22 00:00:14 +0600

    a

--- m6/33/index.js
+++ m6/33/index.js
@@ -1,6 +1,23 @@
 const user = {id: 101, name: "Shahadat", job: 'Student'}
-
 const stringified = JSON.stringify(user)
 
-console.log(user);
-console.log(stringified);
+// console.log(user);
+// console.log(stringified);
+
+const shop = {
+    name: 'Dipok Shop',
+    address: 'Foridpur',
+    profit: 15400,
+    products: ['laptop','onion','pepsi'],
+    isExpensive: false
+}
+
+// sting to JSON
+const shopStringified = JSON.stringify(shop);
+
+// console.log(shop);
+console.log(shopStringified);
+
+//JSON to string
+const converted = JSON.parse(shopStringified)
+console.log(converted);
