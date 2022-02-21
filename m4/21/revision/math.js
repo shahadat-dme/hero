dd250cac1e90a62414f8fcf2419a18c8c504674e
Shahadat <shahadat2cp17@gmail.com>
2022-02-21 19:54:15 +0600

    swap

--- m4/21/revision/math.js
+++ m4/21/revision/math.js
@@ -18,3 +18,25 @@ console.log(output3);
 const num4 = 6.778;
 const output4 = Math.round(num4)
 console.log(output4);
+
+
+const output5 = Math.random();
+console.log(output5);
+
+const output6 = Math.random() * 10;
+console.log(output6);
+
+const output7 = Math.random() * 1000;
+const rounded = Math.floor(output7)
+console.log(rounded);
+
+const output8 = Math.random() * 6;
+const rounded1 = Math.round(output8)
+console.log(rounded1);
+
+// Six
+for(let i= 0; i <= 20; i++){
+    const output8 = Math.random() * 6;
+    const rounded1 = Math.round(output8)
+    console.log(rounded1);
+}
---
+++ m4/21/revision/swap.js
@@ -0,0 +1,14 @@
+// var first = 5;
+// var second = 7;
+
+// var temp = first;
+// first = second;
+// second = temp;
+
+// console.log(first, second);
+
+
+var a = 5;
+var b = 7;
+[a,b] = [b,a]
+console.log(a, b);
