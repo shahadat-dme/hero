22702f56307eb3678bbe2b67d1cc26ba459a1285
Shahadat <shahadat2cp17@gmail.com>
2022-02-21 19:23:38 +0600

    math

---
+++ m4/21/revision/math.js
@@ -0,0 +1,20 @@
+const num = -5;
+const output = Math.abs(num)
+console.log(output);
+
+
+const num1 = 6.678;
+const output1 = Math.ceil(num1)
+console.log(output1);
+
+const num2 = 6.078;
+const output2 = Math.floor(num2)
+console.log(output2);
+
+const num3 = 6.078;
+const output3 = Math.round(num3)
+console.log(output3);
+
+const num4 = 6.778;
+const output4 = Math.round(num4)
+console.log(output4);
