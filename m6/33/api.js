679d53003d9eabe8bb8d884ced8ebadf901b6ab6
Shahadat <shahadat2cp17@gmail.com>
2022-02-22 16:15:47 +0600

    json api

--- m6/33/api.js
+++ m6/33/api.js
@@ -20,11 +20,22 @@ function loadPost(){
     .then(data => console.log(data))
 }
 
+
+
 function loadUser2(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(data => displayUsers(data))
 }
 function displayUsers(data){
-    console.log(data);
+    // console.log(data);
+    const ul = document.getElementById('users')
+    for(const user of data){
+        console.log(user.name);
+        const li = document.createElement('li')
+        // li.innerText = 'Ami new Users';
+        // li.innerText = user.name;
+        li.innerText = `name: ${user.name} Email: ${user.email}`;
+        ul.appendChild(li)
+    }
 }
