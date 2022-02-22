a092d0840ee481e2d8e0de292c7aca2558bd768b
Shahadat <shahadat2cp17@gmail.com>
2022-02-23 00:40:51 +0600

    rest

--- m6/34/README.md
+++ m6/34/README.md
@@ -1,3 +1,6 @@
-# API: https://kanye.rest/
 # API: https://jsonplaceholder.typicode.com/
+# API: https://kanye.rest/
 # API: https://randomuser.me/
+# Api: https://picsum.photos/400/300
+# with id: https://picsum.photos/id/1/200/300
+# API: https://restcountries.com/
---
+++ m6/34/buddy.js
@@ -0,0 +1,17 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayBuddies(data))
+}
+loadBuddies()
+const displayBuddies = data => {
+    const buddies = data.results;
+    // console.log(data.results);
+    const buddiesDiv = document.getElementById('buddies')
+    for(const buddy of buddies){
+        console.log(buddy.email);
+        const p = document.createElement('p')
+        p.innerText = `${buddy.name.first} ${buddy.name.last}  Email: ${buddy.email}`;
+        buddiesDiv.appendChild(p)
+    }
+}
