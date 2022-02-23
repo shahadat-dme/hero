217cada44c36d15f0365caa0c88096cbdebc40b0
Shahadat <shahadat2cp17@gmail.com>
2022-02-23 23:54:16 +0600

    api

--- practice/api/api.js
+++ practice/api/api.js
@@ -1,6 +1,16 @@
 fetch('https://api.chucknorris.io/jokes/random')
 .then(response => {
-    console.log(response);
+    return response.json();
+    // console.log("API:",response.json());
+})
+.then((data) => {
+    // console.log("DATA is: ", data);
+
+    const joke = data.value;
+    console.log("JOKE: " ,joke );
+    
+    const id = data.id
+    console.log("ID: ", id);
 })
 // .then()
 // .then()
