02749c402c2beba166781031ef43ac463f60500e
Shahadat <shahadat2cp17@gmail.com>
2022-02-25 08:42:39 +0600

    null

--- m6/35/04-null-undefined.js
+++ m6/35/04-null-undefined.js
@@ -1,8 +1,44 @@
+// null means nothing or kicui nai, undefined means not set or define anything
 // let first;
 // console.log(first);
 
-function second(x,y){
-    const sum = x + y;
-}
-const result = second(3,91);
-console.log(result);
+// function second(x,y){
+//     const sum = x + y;
+// }
+// const result = second(3,91);
+// console.log(result);
+
+// function add(a,b){
+//     const sum = a + b;
+//     // return;
+//     if(b < 10){
+//         return
+//     }
+//     const fun = a * b;
+//     return sum;
+// }
+// const fourth = add(2,7)
+// console.log(fourth);
+
+// function double(a,b){
+//     const result = a * 2;
+//     console.log(b);
+//     return result;
+// }
+// double(81);
+
+// const fifth = {name: 'Shahadat', age: 15, location: 'Sonaimuri'}
+// console.log(fifth.phone);
+
+// const sixth = [54,12,41,8]
+// console.log(sixth);
+
+// const sixth = [54,12,41,8]
+// delete sixth[2];
+// console.log(sixth[2]);
+
+// const x = undefined;
+// console.log(x);
+
+const myObj= {name: 'Samad', job: null}
+console.log(myObj);
