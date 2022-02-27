d1007ae5b9b5804823ff15c929d6c13fc17448b4
Shahadat <shahadat2cp17@gmail.com>
2022-02-27 16:37:41 +0600

    api

--- m6/36.5/api.js
+++ m6/36.5/api.js
@@ -1,15 +1,30 @@
-const loadSingleUser = () => {
-    fetch('https://randomuser.me/api/')
-    .then(res => res.json())
-    // .then(data => console.log(data.results[0]))
-    .then(data => console.log(data.results[0]))
-}
-loadSingleUser()
+// const loadSingleUser = () => {
+//     fetch('https://randomuser.me/api/')
+//     .then(res => res.json())
+//     // .then(data => console.log(data.results[0]))
+//     .then(data => console.log(data.results[0]))
+// }
+// loadSingleUser()
 
-const displaySingleUser = user =>{
-    console.log(user);
-}
+// const displaySingleUser = user =>{
+//     console.log(user);
+// }
 
+// meal db
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+const toggleSearchResult = displayStyle => {
+    document.getElementById('meals').style.display = displayStyle;
+}
+const searchMeal = () => {
+    const searchText = document.getElementById('search-field').value;
+    //show spinner
+    toggleSpinner('block')
+    toggleSearchResult('none')
+    loadMeals(searchText)
+    document.getElementById('search-field').value = '';
+}
 const loadMeals = searchText => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
@@ -19,15 +34,23 @@ const loadMeals = searchText => {
 }
 
 const displayMeals = meals => {
+    
     const container = document.getElementById('meals');
-    meals.forEach(meal => {
+    container.textContent = '';
+    // if(!meals){
+    //     return 'You have no meals'
+    // }
+    meals?.forEach(meal => {
         const div = document.createElement('div');
         div.innerHTML = `
         <h1>${meal.strMeal} </h1>
+        <p>${meal.strIngredient8 ? meal.strIngredient8: ''}</p>
         <button onclick="loadMealDetails('${meal.strMeal}')">Click Me</button>
         `;
         container.appendChild(div)
     })
+    toggleSpinner('none')
+    toggleSearchResult('block')
 }
 
 loadMeals('fish')
