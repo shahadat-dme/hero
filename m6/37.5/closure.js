bb22116fafb752295d784d64658b64dfdff44526
Shahadat <shahadat2cp17@gmail.com>
2022-03-03 00:30:03 +0600

    closure

--- m6/37.5/closure.js
+++ m6/37.5/closure.js
@@ -1,13 +1,24 @@
 const bank = owner => {
     balance = 0;
-    return amount => {
-        balance += amount
+    return {
+        deposit: amount => {
+            balance += amount
+            return balance
+    },
+    withdraw: amount => {
+        balance -= amount
         return balance
     }
 }
+}
 
 const MofiBank = bank('Mofij');
-console.log(MofiBank(100));
-console.log(MofiBank(200));
-console.log(MofiBank(400));
-console.log(MofiBank(500));
+console.log(MofiBank.deposit(100));
+console.log(MofiBank.deposit(200));
+console.log(MofiBank.deposit(400));
+console.log(MofiBank.deposit(500));
+// console.log(MofiBank.balance);
+console.log(MofiBank.deposit(200));
+
+console.log(MofiBank.withdraw(200));
+console.log(MofiBank.withdraw(400));
