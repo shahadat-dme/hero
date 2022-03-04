dc689afdc331d05d7b5aa7b74eed2fc0ede738f3
Shahadat <shahadat2cp17@gmail.com>
2022-03-04 20:05:16 +0600

    39

---
+++ m7/39/summary.js
@@ -0,0 +1,17 @@
+alert('');
+confirm('are you want?');
+prompt('name')
+
+// http://127.0.0.1:5500/m7/39/07-index.html
+
+// http -> protocol
+
+location.reload();
+location.hash = '';
+location.href = '#';
+
+history.back();
+history.go();
+
+// cookies vs sessionStorage vs localStorage
+// browser api vs web api 
