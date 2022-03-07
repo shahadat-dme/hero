9bab65ffa56419fa5d5bbcf548b8ff652b51f220
Shahadat <shahadat2cp17@gmail.com>
2022-03-07 16:52:52 +0600

    quiz

---
+++ m7/41/quiz.js
@@ -0,0 +1,31 @@
+// const marks= 60;
+// const resubmit = -5; 
+// console.log(!(marks > 0 || resubmit > 0));
+
+
+const person = {name: "hero", id: 101, address: "BD",postalCode: 40321};
+// console.log(Object.keys(person).length);
+// console.log(Object.keys.length);
+
+
+const animals = ['cat', 'dog', 'rat'];
+// console.log(animals.includes('Cat'));
+
+
+const unique = array => array.indexOf("21");
+let unique1 = unique([2,3,4,5,2])
+// console.log(unique1);
+
+
+const add =(para1 , para2) => para2 + para1
+
+let add1 = add("2",3)
+// console.log(add1);
+
+
+// const b=6 , b=6
+// console.log(b);
+
+
+const colors = {mango: 'green', grapes: 'black', organe: 'yellow'};
+console.log(colors[grapes])
