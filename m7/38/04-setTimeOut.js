f641768d1fded18c3b0d1f903dbc54345667e7cb
Shahadat <shahadat2cp17@gmail.com>
2022-03-08 23:36:27 +0600

    settimeout

---
+++ m7/38/04-setTimeOut.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+
+function doSomeTheing(){
+    console.log('Code');
+}
+
+setTimeout(doSomeTheing, 4000)
+
+setTimeout(() => {
+    console.log('Pro');
+},2000)
+console.log("World!");
+
