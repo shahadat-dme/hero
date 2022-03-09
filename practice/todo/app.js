6ca9ff3f679eb5514fe1a2c8b4bdd39ce70201b6
Shahadat <shahadat2cp17@gmail.com>
2022-03-09 15:13:32 +0600

    todo

---
+++ practice/todo/app.js
@@ -0,0 +1,55 @@
+const displayLocalStorageCart = () => {
+    const cart = getCart();
+    for(const name in cart){
+        displayProduct(name)
+    }
+}
+
+const addItem = () => {
+    const nameField = document.getElementById('product-name');
+    const name = nameField.value;
+
+    if(!name){
+        return;
+    }
+    displayProduct(name)
+    addProductToCart(name);
+    nameField.value = '';
+}
+
+const displayProduct = name => {
+    const ul = document.getElementById('products');
+    const li = document.createElement('li')
+    li.innerText = name;
+    ul.appendChild(li);
+}
+
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if(cart){
+        cartObj = JSON.parse(cart)
+    }else {
+        cartObj = {}
+    }
+    return cartObj;
+}
+
+const addProductToCart = name => {
+    const cart = getCart();
+    // cart[name] = 1;
+    // console.log(cart);
+    if(cart[name]){
+        cart[name] = cart[name] + 1;
+    }
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem('cart', cartStringified);
+    
+}
+
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart')
+}
+
+displayLocalStorageCart();
