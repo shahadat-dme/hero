5aa0158e6c2ab8b7b983816cafcc19ad409c2c09
Shahadat <shahadat2cp17@gmail.com>
2022-03-13 22:47:47 +0600

    string

---
+++ re-intro/string/01.js
@@ -0,0 +1,9 @@
+console.log('hello'.length);
+console.log('hello world!'.length);
+
+'hello'.charAt(0); // "h"
+'hello, world'.replace('world', 'mars'); // "hello, mars"
+'hello'.toUpperCase(); // "HELLO"
+
+Boolean('');  // false
+Boolean(234); // true
