38313d8fdd41690c72d91f5b0bc59b5414affb94
Shahadat <shahadat2cp17@gmail.com>
2022-03-14 07:48:27 +0600

    operator

---
+++ re-intro/04-operator.js
@@ -0,0 +1,10 @@
+// x += 5;
+// x = x + 5;
+console.log('hello' + ' world' ); // "hello world"
+console.log('3' + 4 + 5);
+console.log( 3 + 4 + '5');
+
+console.log(123 == '123');
+console.log(1 == true);
+console.log(123 === '123');
+console.log(1 === true);
