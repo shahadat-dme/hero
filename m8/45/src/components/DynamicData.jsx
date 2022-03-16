0dd3feb82e554fd5922039c97e32d167521ba256
Shahadat <shahadat2cp17@gmail.com>
2022-03-16 11:47:13 +0600

    DynamicData

--- m8/45/src/components/DynamicData.jsx
+++ m8/45/src/components/DynamicData.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function DynamicData() {
+
+    const [users, setUsers] = useState([])
+    // useEffect(()=> {},[])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(data => setUsers(data))
+    }, [])
+
     return (
-        <div>DynamicData</div>
+        <div>
+            <h2>DynamicData</h2>
+            <p>{users.length}</p>
+            {
+                users.map(user => <li>{user.name}</li>)
+            }
+
+            {
+                users.map(user => <User name={user.name} email={user.email} />)
+            }
+        </div>
+    )
+}
+
+function User(props) {
+    return (
+        <div>
+            <h2>Name: {props.name}</h2>
+            <h4>Email: {props.email}</h4>
+        </div>
     )
 }
 export default DynamicData;
