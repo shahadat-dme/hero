eda1417df0cda5ace93b3e882abaa2a7185451c8
Shahadat <shahadat2cp17@gmail.com>
2022-03-18 15:19:25 +0600

    drag and drop

--- practice/drag-drop/app.js
+++ practice/drag-drop/app.js
@@ -13,7 +13,30 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
     container.addEventListener('dragover', e => {
         e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
         const draggable = document.querySelector('.dragging')
-        container.appendChild(draggable)
+        // console.log(afterElement);
+        if(afterElement == null){
+            container.appendChild(draggable)
+        }else {
+            container.insertBefore(draggable, afterElement)
+        }
+        // container.appendChild(draggable)
     })
 })
+
+function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        // console.log(offset);
+        if(offset < 0 && offset > closest.offset){
+            return {offset: offset, element: child}
+        }
+        else {
+            return closest
+        }
+    },{offset: Number.POSITIVE_INFINITY}).element
+}
