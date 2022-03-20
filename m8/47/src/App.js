63d4a13f4270f0b2573edf6169e9586aeff7136a
Shahadat <shahadat2cp17@gmail.com>
2022-03-20 21:00:27 +0600

    cosmetics

--- m8/47/src/App.js
+++ m8/47/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import Device from './components/Device/Device';
+import Cosmetics from './components/Cosmetics/Cosmetics';
+// import Device from './components/Device/Device';
 // import Dial from './components/Dial/Dial';
-import Watch from './components/Watch/Watch';
+// import Watch from './components/Watch/Watch';
 
 function App() {
   return (
     <div className="App">
-      <h1>Shahadat</h1>
-      <Device name="ph" price="780"/>
-      <Watch/>
+      {/* <h1>Shahadat</h1> */}
+      {/* <Device name="ph" price="780"/> */}
+      {/* <Watch/> */}
       {/* <Dial/> */}
+      <Cosmetics/>
     </div>
   );
 }
---
+++ m8/47/src/components/Cosmetics/Cosmetics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>Shahadat</h1>
+        </div>
+    )
+}
+
+export default Cosmetics
