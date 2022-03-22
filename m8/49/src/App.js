8b3d4ddadd9daf40bcd78a96bc7741ad2b8f6f12
Shahadat <shahadat2cp17@gmail.com>
2022-03-22 23:01:07 +0600

    49

---
+++ m8/49/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+
+function App() {
+  return (
+    <div className="">
+      <Header/>
+      <Shop/>
+    </div>
+  );
+}
+
+export default App;
