49a5a230505300340f93a38c3f83c05eee38d985
Shahadat <shahadat2cp17@gmail.com>
2022-03-23 12:39:02 +0600

    49

--- m8/49/src/components/Shop/Shop.jsx
+++ m8/49/src/components/Shop/Shop.jsx
@@ -58,7 +58,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart} />
             </div>
         </div>
     );
