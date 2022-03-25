0725f42888188bdfeda4b1a4d16d2c7023123567
Shahadat <shahadat2cp17@gmail.com>
2022-03-25 11:06:05 +0600

    code

---
+++ js/core/01-destructuring.js
@@ -0,0 +1,7 @@
+const personObject = {
+    firstName: 'Shahadat',
+    lastName: "Hossain",
+    address: {country: "Bangladesh", capital: 'Dhaka'}
+};
+const cart = {abc: 1, def: 2, ghi: 1}
+const personArray= ["Shahadat","Hossain"]
