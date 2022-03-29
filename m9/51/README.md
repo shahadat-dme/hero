52fcdece3d26f3ba8173914018f3d74d697c92b3
Shahadat <shahadat2cp17@gmail.com>
2022-03-29 10:45:33 +0600

    tailwind

--- m9/51/README.md
+++ m9/51/README.md
@@ -1,4 +1,5 @@
 # Getting Started with Create React App
+"bootstrap": "^5.1.3",
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- m9/51/src/App.js
+++ m9/51/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Bootstrap from './Bootstrap';
+// import Bootstrap from './Bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <Bootstrap/>
+    <div className="">
+      {/* <Bootstrap/> */}
+      <h1>Hello Tailwind</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
