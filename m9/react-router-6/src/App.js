1e1c77ebc7e25b65fcf382eeb61701eb13fb8aae
Shahadat <shahadat2cp17@gmail.com>
2022-04-03 16:38:20 +0600

    router 6

--- m9/react-router-6/src/App.js
+++ m9/react-router-6/src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
+import { useState } from 'react'
+
+// components
+import Home from './components/Home'
+import About from './components/About'
+import Products from './components/Products'
+import ProductDetails from './components/ProductDetails'
+import NotFound from './components/NotFound'
 
 function App() {
+  const [cartIsEmpty] = useState(false)
+
   return (
     <div className="App">
+      
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/*" element={<About />} />
+          <Route path="/products/:id/*" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/test" element={(
+            <div>
+              <h2>Test page</h2>
+            </div>
+          )} />
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+          <Route 
+            path="/checkout" 
+            element={cartIsEmpty ? <Navigate to="/products" /> : <p>checkout</p>}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
