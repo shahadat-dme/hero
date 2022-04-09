49f710bf0e683e5300149251726410950d75324f
Shahadat <shahadat2cp17@gmail.com>
2022-04-09 20:21:48 +0600

    56

--- m10/56/src/App.js
+++ m10/56/src/App.js
@@ -1,25 +1,44 @@
 import './App.css';
 import app from './firebase.init';
-import {getAuth, GithubAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GithubAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GithubAuthProvider();
 
-  const handleGoogleSignIn = () => {
+  const handleSignIn = () => {
     signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch((error) => {
       console.error('error', error);
     })
   }
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUser({})
+    })
+    .catch(error => {
+      setUser({});
+    })
+  }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google SignIn</button>
+      {
+        user.email ? <button onClick={handleSignOut}> SignOut </button> :
+        <button onClick={handleSignIn}> SignIn </button>
+      }
+      
+      <h2>Name: {user.displayName}</h2>
+      <p>Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
