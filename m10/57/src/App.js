60f73f231160c64357e43d9231472538729d051f
Shahadat <shahadat2cp17@gmail.com>
2022-04-10 14:25:25 +0600

    react 18 add

--- m10/57/src/App.js
+++ m10/57/src/App.js
@@ -7,8 +7,30 @@ import { getAuth } from "firebase/auth";
 const auth = getAuth(app);
 
 function App() {
+  const handleEmailChange = (event) => {
+    // console.log('Something Typed');
+    console.log(event.target.value);
+  }
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value);
+  }
+  const handlePassworwBlur = (event) => {
+    console.log(event.target.value);
+  }
+  const handleFormSubmit = event => {
+    console.log('form submitted');
+    event.preventDefault();
+  }
   return (
     <div className="App">
+      <form onSubmit={handleFormSubmit}>
+        {/* <input onChange={handleEmailChange} type='email' name='' id=''/> */}
+        <input onBlur={handleEmailBlur} type='email' name='' id=''/>
+        <br/>
+        <input onBlur={handlePassworwBlur} type='password' name='' id=''/>
+        <br/>
+        <input type='submit' value='Login'/>
+      </form>
     </div>
   );
 }
