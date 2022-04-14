74b0910de9970eabfe1d7ffdc0ffcf610cd52a2d
Shahadat <shahadat2cp17@gmail.com>
2022-04-14 12:41:03 +0600

    58

--- m10/58/src/components/Login/Login.jsx
+++ m10/58/src/components/Login/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase
     return (
         <div>
             <h2>Please Login</h2>
             <div style={{ margin: '20px' }}>
-                <button>Google Sign In</button>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
             </div>
             <form>
                 <input type='email' placeholder='Your Email' />
