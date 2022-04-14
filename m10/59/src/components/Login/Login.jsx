5ad59bb69dd990b213bfbe50b0a25140d044028f
Shahadat <shahadat2cp17@gmail.com>
2022-04-14 23:55:51 +0600

    59

---
+++ m10/59/src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <h2 className='form-title'>LogIn</h2>
+        </div>
+    )
+}
+
+export default Login
---
+++ m10/59/src/components/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SignUp.css'
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>SignUp</h2>
+        </div>
+    )
+}
+
+export default SignUp
