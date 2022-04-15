77d1a931b348c4ed408ac7388927edfc08294db0
Shahadat <shahadat2cp17@gmail.com>
2022-04-15 23:52:43 +0600

    59

--- m10/59/src/__Auth_steps.js
+++ m10/59/src/__Auth_steps.js
@@ -8,13 +8,16 @@
 7. npm install --save react-firebase-hooks
 8. useCreateUserWithEmailAndPassword form react-firebase-hooks
 9. if user is create redirect to the expected page
-10. useSignInWithEmailAndPassword for sign in
+10. useSignInWithEmailAndPassword for LogIn
+11. create RequireAuth component => check user exists also tract user location
+12. In route wrap Protected Component by useing RequireAuth component
 */
 
 /*
-hosting steps
-1. npm install -g firebase-tools
-2. firebase login
-3. firebase init
-4. firebase deploy
+firebase hosting steps
+1. npm install -g firebase-tools (one time for your computer)
+2. firebase login (one time for your computer)
+3. firebase init (one time for each project )
+4. npm run build (every time you want to deploy)
+5. firebase deploy (every time you want to deploy)
 */ 
