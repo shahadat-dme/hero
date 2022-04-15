23ecf5550b4e9ce51f597b8ca3d32ab2ab53afbe
Shahadat <shahadat2cp17@gmail.com>
2022-04-15 23:11:43 +0600

    RequireAuth

--- m10/59/src/__Auth_steps.js
+++ m10/59/src/__Auth_steps.js
@@ -1,9 +1,14 @@
 /*
-1. create a new project in console.google.com
+1. create a new project in console.firebase.google.com
 2. npm install firebase
 3. create firebase.init.js and import getAuth to export auth
 4. firebase setting -> Authentication -> enable Email and password auth
-5. 
+5. create Login, SignUp , setup route
+6. attach form field handler and form submit handler
+7. npm install --save react-firebase-hooks
+8. useCreateUserWithEmailAndPassword form react-firebase-hooks
+9. if user is create redirect to the expected page
+10. useSignInWithEmailAndPassword for sign in
 */
 
 /*
