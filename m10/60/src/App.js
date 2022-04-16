028e150d25ff8045989d44aa9ca2b7331d7514d6
Shahadat <shahadat2cp17@gmail.com>
2022-04-16 20:11:24 +0600

    60.2

--- m10/60/src/App.js
+++ m10/60/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        <Route path='/' element={}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/' element={}></Route>
+      </Routes>
     </div>
   );
 }
