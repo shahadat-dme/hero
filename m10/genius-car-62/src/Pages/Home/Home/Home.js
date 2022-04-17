9b3cd3191d040da1f42bb86955716efbdcf0347b
Shahadat <shahadat2cp17@gmail.com>
2022-04-17 12:13:34 +0600

    62

---
+++ m10/genius-car-62/src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Experts from '../Experts/Experts';
+import Services from '../Services/Services';
+
+const Home = () => {
+    return (
+        <>
+            <Banner></Banner>
+            <Services></Services>
+            <Experts></Experts>
+        </>
+    );
+};
+
+export default Home;
