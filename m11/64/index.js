ac273898b158984184d6123dfb9a7c5928fe9dad
Shahadat <shahadat2cp17@gmail.com>
2022-04-24 12:15:17 +0600

    64

--- m11/64/index.js
+++ m11/64/index.js
@@ -1,9 +1,71 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!, I am form BD')
+})
+app.get('/about', (req, res) => {
+    res.send({id: 1, name: 'shahadat', job: 'Dev'})
+})
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: '0178888888' },
+    { id: 2, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '0178888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '0178888888' },
+    { id: 4, name: 'suchonda', email: 'suchonda@gmail.com', phone: '0178888888' },
+    { id: 5, name: 'srabonti', email: 'srabonti@gmail.com', phone: '0178888888' },
+    { id: 6, name: 'sabila', email: 'sabila@gmail.com', phone: '0178888888' },
+    { id: 7, name: 'sohana', email: 'sohana@gmail.com', phone: '0178888888' },
+]
+
+// app.get('/users', (req, res) =>{
+//     res.send(users);
+// })
+
+
+// filter by search query parameter
+app.get('/users', (req, res) => {
+    if(req.query.name){
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+app.get('/user/:id', (req, res) =>{
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user);
+});
+
+
+app.post('/user', (req, res) =>{
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
+});
+
+app.get('/fruits', (req, res) =>{
+    res.send(['mango', 'apple', 'oranges']);
+});
+
+app.get('/fruits/mango/fazle', (req, res) =>{
+    res.send('sour sour fazle flavor');
+});
+
+
+app.listen(port, () => {
+    console.log('Listening to port', port)
 })
 
 app.listen(port, () => {
