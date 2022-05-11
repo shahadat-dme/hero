851268fe694f74e5f7878c9cea7a222b972b145f
Shahadat <shahadat2cp17@gmail.com>
2022-05-12 00:50:15 +0600

    71

--- m11/65/index.js
+++ m11/65/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// user: dbuser1
+// password: mtl3ZIkbYtT9H4gt
+
+
+const uri = "mongodb+srv://dbuser1:mtl3ZIkbYtT9H4gt@cluster0.e8p1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("foodExpress").collection("users");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- m12/71/src/App.js
+++ m12/71/src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route/>
       </Routes>
     </div>
