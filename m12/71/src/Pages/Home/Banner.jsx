1083aa8eaa15b69b2b6251eda750df3aa972d8e2
Shahadat <shahadat2cp17@gmail.com>
2022-05-12 11:22:57 +0600

    71

--- m12/71/src/Pages/Home/Banner.jsx
+++ m12/71/src/Pages/Home/Banner.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import chair from '../../assets/images/chair.png'
+import PrimaryButton from '../Shared/PrimaryButton';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen px-12">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <h1 class="text-5xl font-bold">Your Smile Start Here!</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <PrimaryButton>Get Started</PrimaryButton>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Banner
+export default Banner;
---
+++ m12/71/src/Pages/Home/InfoCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const InfoCard = ({ img, cardTitle, bgClass, cardBody }) => {
+    return (
+        <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+            <figure className='pl-5'>
+                <img src={img} alt="Album" />
+            </figure>
+            <div className="card-body text-white">
+                <h2 className="card-title">{cardTitle}</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                {/* <p>{cardBody}</p> */}
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
