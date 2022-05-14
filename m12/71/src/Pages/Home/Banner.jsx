a97d7979446f97b16a8f66ec281d22bd27c4ebeb
Shahadat <shahadat2cp17@gmail.com>
2022-05-14 06:48:24 +0600

    71 com

--- m12/71/src/Pages/Home/Banner.jsx
+++ m12/71/src/Pages/Home/Banner.jsx
@@ -4,7 +4,7 @@ import PrimaryButton from '../Shared/PrimaryButton';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen px-12">
+        <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
--- m12/71/src/Pages/Home/Home.jsx
+++ m12/71/src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Banner from './Banner'
+import Contact from './Contact'
+import Footer from './Footer'
 import Info from './Info'
 import MakeAppointment from './MakeAppointment'
 import Services from './Services'
+import Testimonials from './Testimonials'
 
 const Home = () => {
     return (
@@ -11,6 +14,9 @@ const Home = () => {
             <Info />
             <Services />
             <MakeAppointment />
+            <Testimonials />
+            <Contact />
+            <Footer />
         </div>
     )
 }
