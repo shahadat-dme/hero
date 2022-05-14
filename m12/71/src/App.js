2019df1ba2c24fb8bca78ab64b6bc6db8f5b6b6f
Shahadat <shahadat2cp17@gmail.com>
2022-05-15 00:43:44 +0600

    71

--- m12/71/src/App.js
+++ m12/71/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 // import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/appointment' element={<Appointment/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route/>
       </Routes>
