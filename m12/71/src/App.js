114f9b6e1962c23da5865f65c4bfd512e04e0ab7
Shahadat <shahadat2cp17@gmail.com>
2022-05-17 10:55:31 +0600

    73

--- m12/71/src/App.js
+++ m12/71/src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
 import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -14,7 +15,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
-        <Route path='/appointment' element={<Appointment/>}/>
+        <Route path='/appointment' element={
+        <RequireAuth>
+          <Appointment/>
+        </RequireAuth>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route/>
--- m12/71/src/Pages/Appointment/BookingModal.jsx
+++ m12/71/src/Pages/Appointment/BookingModal.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
+    const [user, loading, error] = useAuthState(auth);
 
     const handleBooking = event => {
         event.preventDefault();
@@ -15,22 +18,25 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
 
     return (
         <div>
-            <input type="checkbox" id="booking-modal" class="modal-toggle" />
-            <div class="modal modal-bottom sm:modal-middle">
-                <div class="modal-box">
-                    <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="font-bold text-lg text-secondary">Booking for: {name}</h3>
+            <input type="checkbox" id="booking-modal" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
                     <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
-                        <input type="text" disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
-                        <select name="slot" class="select select-bordered w-full max-w-xs">
+                        <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
+                        <select name="slot" className="select select-bordered w-full max-w-xs">
                             {
-                                slots.map(slot => <option value={slot}>{slot}</option>)
+                                slots.map((slot, index) => <option
+                                    key={index}
+                                    value={slot}
+                                >{slot}</option>)
                             }
                         </select>
-                        <input type="text" name="name" placeholder="Your Name" class="input input-bordered w-full max-w-xs" />
-                        <input type="email" name="email" placeholder="Email Address" class="input input-bordered w-full max-w-xs" />
-                        <input type="text" name="phone" placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
-                        <input type="submit" value="Submit" class="btn btn-secondary w-full max-w-xs" />
+                        <input type="text" name="name" disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
                 </div>
             </div>
